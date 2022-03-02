1c6047fbcb590a34dd6430eb3a6bd4e11c9f5f69
kim-hyunjin <hyunjin1612@gmail.com>
2022-03-02 12:18:43 +0900

    add component and stories

---
+++ src/components/Task.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Task({
+  task: { id, title, state },
+  onArchiveTask,
+  onPinTask,
+}) {
+  return (
+    <div>
+      <input type="text" value={title} readOnly={true} />
+    </div>
+  );
+}
