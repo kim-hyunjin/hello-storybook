a310429cc027ae32374244a053fa345c9b7381c2
kim-hyunjin <hyunjin1612@gmail.com>
2022-07-01 12:38:50 +0900

    config and deploy chromatic

--- src/components/Loading.stories.js
+++ src/components/Loading.stories.js
@@ -3,15 +3,15 @@ import React from "react";
 import Loading from "./Loading";
 
 export default {
-    component: Loading,
-    title: "Loading",
+  component: Loading,
+  title: "Loading",
 };
 
 const Template = (args) => <Loading {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {
-    size: 32,
-    lineColor: "black",
-    backgroundColor: "#26c6da",
+  size: 30,
+  lineColor: "black",
+  backgroundColor: "#26c6da",
 };
